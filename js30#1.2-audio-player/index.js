1c69ca4f5a3f66278010392bece744271fed7e74
Marina Kholodova <marinaholodova@gmail.com>
2024-08-30 00:38:23 +0300

    feat: add audio-player functionality

---
+++ js30#1.2-audio-player/index.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const songs = [
+		{
+			id: "1",
+			artist: "Beyonce",
+			name: "Don't Hurt Yourself",
+			song: "./assets/audio/beyonce.mp3",
+			img: './assets/img/lemonade.png'
+		},
+		{
+			id: "2",
+			artist: "Dua Lipa",
+			name: "Don't Start Now",
+			song: "./assets/audio/dontstartnow.mp3",
+			img: './assets/img/dontstartnow.png'
+		}
+	];
+	const bodyElement = document.body;
+	const info = document.querySelector('.audio-player__info');
+	const wrapper = document.querySelector('.wrapper');
+	const playPause = document.querySelector('.playPause');
+	const next = document.querySelector('.nextPrev_next');
+	const prev = document.querySelector('.nextPrev_prev');
+	const audio = new Audio();
+	let isPlay = false;
+	let currentSongIndex = 0;
+
+	/*
+			console.log('playPause:', playPause);
+		console.log('next:', next);
+		console.log('prev:', prev);
+	*/
+
+
+	playPause.addEventListener('click', togglePlay);
+	next.addEventListener('click', playNextSong);
+	prev.addEventListener('click', playPrevSong)
+
+	function togglePlay() {
+		if (!isPlay) {
+			playPause.classList.add('playPause_pause');
+			playAudio();
+		} else {
+			playPause.classList.remove('playPause_pause');
+			pauseAudio();
+		}
+	}
+
+	function playNextSong() {
+		currentSongIndex = currentSongIndex < songs.length - 1 ? currentSongIndex + 1 : 0;
+		updatePlayer(songs[currentSongIndex]);
+		isPlay = false;
+		togglePlay();
+	}
+
+	function playPrevSong() {
+		currentSongIndex = currentSongIndex > 0 ? currentSongIndex - 1 : songs.length - 1;
+		updatePlayer(songs[currentSongIndex]);
+		isPlay = false;
+		togglePlay();
+	}
+
+	function updatePlayer(song) {
+		bodyElement.style.backgroundImage = `url(${song.img})`;
+		wrapper.querySelector('.audio-player__img').src = song.img;
+		wrapper.querySelector('.audio-player__artist').textContent = song.artist;
+		wrapper.querySelector('.audio-player__song').textContent = song.name;
+		audio.src = song.song;
+	}
+
+	function playAudio() {
+		audio.currentTime = 0;
+		audio.play();
+		isPlay = true;
+	}
+
+	function pauseAudio() {
+		audio.pause();
+		isPlay = false;
+	}
+
+	updatePlayer(songs[0]);
+
+});
